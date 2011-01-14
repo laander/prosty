9ff43d026a2af55c4bcd5ea235b02d8760e28994
laander <la@laander.com>
2011-01-14 16:14:59 +0100

    changed gitignore, updated README and made role model use static array for datasource for optimizations

--- app/models/role.php
+++ app/models/role.php
@@ -1,6 +1,36 @@
 <?php
 class Role extends AppModel {
     var $name = 'Role';
+	
+	// Datasource for roles, uses data in static array instead of db lookup for optimization
+	var $useDbConfig = 'array';	
+	var $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Angel',
+			'modified' => '',
+			'created' => '',
+			'created_by' => 1,
+			'modified_by' => 1
+		),
+		array(
+			'id' => 2,
+			'name' => 'Agent',
+			'modified' => '',
+			'created' => '',
+			'created_by' => 1,
+			'modified_by' => 1
+		),
+		array(
+			'id' => 3,
+			'name' => 'Client',
+			'modified' => '',
+			'created' => '',
+			'created_by' => 1,
+			'modified_by' => 1
+		)
+	);
+
 }
 
 ?>
